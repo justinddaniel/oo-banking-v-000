2a6211241ec5c093b4afff840c68deff1df8f272
Justin Daniel <justinddaniel@gmail.com>
2017-09-28 01:34:01 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,11 @@
 class Transfer
-  # your code here
+
+attr_reader :name1, :name2, :amount
+
+  def initialize(name1, name2, amount)
+    @name1 = name1
+    @name2 = name2
+    @amount = amount
+  end
+  
 end
