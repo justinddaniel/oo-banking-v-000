8fbdf88e04880a12707e7abfb994b254140fa612
Justin Daniel <justinddaniel@gmail.com>
2017-09-28 02:06:18 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,6 +16,7 @@ attr_reader :sender, :receiver, :amount
 
   def execute_transaction
     if self.valid? == false || self.status == "complete" || self.amount > sender.balance
+      self.status = "rejected"
       "Transaction rejected. Please check your account balance."
     else
       sender.balance -= self.amount
